b1574eefba13d666457974ae6853d5b799581e71
jeff <geoffreyerastus956@gmail.com>
2024-01-30 23:21:12 -0500

    packages updates

---
+++ app/src/main/java/com/example/joblinker/dashboard/Home.kt
@@ -0,0 +1,12 @@
+package com.example.joblinker.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.joblinker.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/example/joblinker/screens/Register.kt
@@ -0,0 +1,12 @@
+package com.example.joblinker.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.joblinker.R
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
