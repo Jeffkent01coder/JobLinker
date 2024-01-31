2762abb17fc1066211f5d1300019941dd0caa22a
jeff <geoffreyerastus956@gmail.com>
2024-01-30 23:31:12 -0500

    splash

--- app/src/main/java/com/example/joblinker/MainActivity.kt
+++ app/src/main/java/com/example/joblinker/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.joblinker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.joblinker.dashboard.Home
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
